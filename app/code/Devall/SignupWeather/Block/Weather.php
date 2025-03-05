570995bd063601df58d080837676b45eb9460724
davidtabat <79899333+davidtabat@users.noreply.github.com>
2025-03-05 21:23:13 +0400

    comments

--- app/code/Devall/SignupWeather/Block/Weather.php
+++ app/code/Devall/SignupWeather/Block/Weather.php
@@ -1,5 +1,5 @@
 <?php
-
+// if it's empty than why we need it? it extends Magento\Framework\View\Element\Template, so since it's empty using this class would be same as using default template class. Which is used by default when u declare block element in xml file.
 declare(strict_types=1);
 
 namespace Devall\SignupWeather\Block;
